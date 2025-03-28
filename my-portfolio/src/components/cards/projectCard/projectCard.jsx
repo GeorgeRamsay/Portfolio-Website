402ca1d2b596a35accd3b8254c27997c1bb0ccdf
GeorgeRamsay <george.a.ramsay.98@gmail.com>
2025-03-28 17:55:27 +1300

    minor additions to card css and js to get working

--- my-portfolio/src/components/cards/projectCard/projectCard.jsx
+++ my-portfolio/src/components/cards/projectCard/projectCard.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import style from './projectCard.module.css';
+import classNames from 'classnames';
+
 import { useNavigate } from 'react-router-dom';
 
 const ProjectCard = ({ image, title, text, link }) => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <img className={style.imageContainer} src={image} alt={title} />
+        <div className={style.card}>
+            <img className={classNames(style.imageContainer)} src={image} alt={title} />
+            <div className={classNames(style.cardCover, style.gradientBlur)}>
+                <h3>{title}</h3>
+                <p>{text}</p>
             
-            <h3>{title}</h3>
-            <p>{text}</p>
-            <button onClick={() => navigate(link)}>View Project</button>
+                <button onClick={() => navigate(link)}>View Project</button>
+            </div>
         </div>
     );
 }
