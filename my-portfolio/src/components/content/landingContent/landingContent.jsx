865c3ed548aba0186003909f6011d86c8a794f27
GeorgeRamsay <george.a.ramsay.98@gmail.com>
2025-04-30 10:11:33 +1200

    file clean ups

--- my-portfolio/src/components/content/landingContent/landingContent.jsx
+++ my-portfolio/src/components/content/landingContent/landingContent.jsx
@@ -3,12 +3,10 @@ import LandingHeader from '../../sections/landingHeader/landingHeader'
 import LandingAbout from '../../sections/landingAbout/landingAbout';
 
 import style from './landingContent.module.css';
-import FadeLayout from '../../../layouts/fadeLayout/fadeLayout';
 import LandingProjects from '../../sections/landingProjects/landingProjects';
 import WorkExperienceSection from '../../sections/workExperienceSection/workExperienceSection';
 import Timeline from '../../animations/timeLine/timeLine';
 import TechStack from '../../sections/techStack/techStack.js';
-import PullAwayComponent from "../../animations/pullAway/pullAway.js";
 
 
 export const LandingContent = () => {
--- my-portfolio/src/pages/landingPage.jsx
+++ my-portfolio/src/pages/landingPage.jsx
@@ -2,7 +2,6 @@ import { LandingContent } from "../components/content/landingContent/landingCont
 import Star from '../components/backgrounds/stars.js';
 import React, { useEffect } from 'react';
 import ProgressBar from "../components/animations/progressBar/progressBar.js";
-import PullAwayComponent from "../components/animations/pullAway/pullAway.js";
 
 function LandingPage() {
 
